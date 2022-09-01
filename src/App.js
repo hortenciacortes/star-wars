c89ddfab2caa70b00b32a358fe00821b944bc7b0
Hortência Côrtes <hortencia.cortes@gmail.com>
2022-08-31 22:45:34 -0300

    adding personages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Personages } from "./components/Personages";
 
 function App() {
   return (
     <>
       <Header />
+      <Personages />
     </>
   );
 }
---
+++ src/components/Personages/index.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import { swapi } from "../../server/api";
+import './styles.css';
+
+export function Personages() {
+  const [people, setPeople] = useState([]);
+  const firstRenderRef = useRef(true);
+
+  useEffect(() => {
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false;
+      return;
+    }
+
+    if(people.length === 0) {
+      for(let i = 1; i <= 9; i++){
+        swapi.get(`/people/?page=${i}`)
+        .then(response => setPeople(prevent => [...prevent, ...response.data.results]))
+        .catch((err) => console.error(err));
+      }
+    }
+  }, [])
+  
+  
+  return(
+    <section className="grid-pattern">
+      <div className="cointainer">
+        <h2>Personagens do universo de Star Wars ({people.length}):</h2>
+        <ul>
+          {people.map((name, index) => (
+            <li key={index}>{name.name}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
